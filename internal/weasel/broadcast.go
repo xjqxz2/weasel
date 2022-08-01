3b24ce325b9ec517eeb95e5252de11a8f2d5eead
xjqxz2 <xjqxz_2@126.com>
2022-08-01 23:12:28 +0800

    Close connecting delete session info

--- internal/weasel/broadcast.go
+++ internal/weasel/broadcast.go
@@ -3,6 +3,11 @@ package weasel
 type BroadcastTarget []Session
 
 func (p BroadcastTarget) Broadcast(message []byte) {
+	//	如果未找到客户端则直接返回，不再进行没有意义的广播
+	if len(p) <= 0 {
+		return
+	}
+
 	for _, client := range p {
 		go func(s Session) {
 			s.Write(message)
